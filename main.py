7013ce86997c91acd63f8c7f37ef44b417e2c96a
LlamaCodrr950 <163625693+ASimpleCoder@users.noreply.github.com>
2025-05-08 16:24:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ import smtplib as smtp
 
 sender = smtp.SMTP("smtp.gmail.com")
 sender.starttls()
-sender.login(os.environ("password"), os.environ("email"))
+sender.login(os.environ["email"], os.environ["password"])
 
 st.title("SparkRide Carwash Easy Bookings")
 st.subheader("Book A Variety Of Car Washes and Get them in just a day")
 st.write("*To book instant car washes (washes in under a hour), please call +91 1234567890")
 
 st.subheader("Book a car wash the for a set day")
-date = st.date_input("Day Of Carwash: ", format="DD/MM/YYYY")
+date = st.date_input("Day Of Carwash: ", format="dd/MM/yyyy")
 time = st.time_input("Time Of Carwash: ")
 parking = st.text_input("Enter Parking Lot Number: ")
 
@@ -21,4 +21,7 @@ selected = st.radio("Select a option to wash your car", ["Simple Microfiber Rub 
 submit_button = st.button("Submit a booking")
 
 if submit_button:
-  sender.sendmail(os.environ("password"), os.environ("reciever"), f"NEW BOOKING ARRIVED \n\n Day: {date}, Time: {time}, Parking Lot Number: {parking} \n Mode: {selected}")
+  sender.sendmail(os.environ["email"], os.environ["reciever"], f"NEW BOOKING ARRIVED \n\n Day: {date}, Time: {time}, Parking Lot Number: {parking} \n Mode: {selected}")
+  st.empty()
+  st.success("✅ Booking submitted! You may now close this tab.")
+  st.stop()
